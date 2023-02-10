2f5a77deaed4fa86e823857679f784e303ffef35
venubreddy <venubreddy@gmail.com>
2023-02-10 11:53:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
 
+resource "aws_instance" "jenkins-instance1" {
+  ami           = "ami-0b0af3577fe5e3532"
+  instance_type = "t2.medium"
+  //subnet_id     = "subnet-0317ab2afcac29463"
+  tags = {
+    Name = "venu-vm"
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  provider "aws" {
+  region = "var.aws_region"
+}
+}
+
+terraform {
+  cloud {
+    organization = "vb-org"
+
+    workspaces {
+      name = "vb-tfm"
+    }
+  }
+}
